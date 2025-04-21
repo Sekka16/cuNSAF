cf01565a0ba88f51c150baa200c7836322f11620
root <root@notebook-devenviron-0326-111554-dingqin-gpu-1lkse0x-launcher-0.notebook-devenviron-0326-111554-dingqin-gpu-1lkse0x.ns-97586f53a56c4307adda2a538fae792b.svc.cluster.local>
2025-04-21 09:57:55 +0000

    4/21 17:58

--- ref/pmpp/reduce/reducev0.cu
+++ ref/pmpp/reduce/reducev0.cu
@@ -1,58 +1,85 @@
 #include <bits/stdc++.h>
 #include <cuda_runtime.h>
 
-// 单个block计算出总和
-__global__ void SimpleSumReductionKernel(float *input, float *output) {
-
-  int idx = 2 * threadIdx.x;
-  
-  for (int stride = 1; stride < blockDim.x; stride <<= 1) {
-    if (idx % stride == 0)
-      input[idx] += input[idx + stride];  
+__global__ void SimpleSumReductionKernel(float* input, float* output) {
+  unsigned int i = 2 * threadIdx.x;
+  for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
+    if (threadIdx.x % stride == 0) {
+      input[i] += input[i + stride];
+    }
     __syncthreads();
   }
-
-  if (idx == 0) {
+  if (threadIdx.x == 0) {
     *output = input[0];
   }
 }
 
-int main() {
-  // 1. 设置blockSize和gridSize
-  const int elemNums = 256;
-  const int blockSize = 256;
-  const int gridSize = (elemNums + blockSize - 1) / blockSize;
-
-  // 2. 分配host端内存
-  float *h_input = (float *)malloc(sizeof(float) * elemNums);
-  float *h_output = (float *)malloc(sizeof(float) * elemNums);
-
-  srand(time(NULL));
-  for (int i = 0; i < elemNums; i++) {
-    h_input[i] = (float)rand() / RAND_MAX;
+__global__ void ConvergentSumReductionKernel(float* input, float* output) {
+  unsigned int i = threadIdx.x;
+  for (unsigned int stride = blockDim.x; stride >= 1; stride /= 2) {
+    if (threadIdx.x < stride) {
+      input[i] += input[i + stride];
+    }
+    __syncthreads();
   }
+  if (threadIdx.x == 0) {
+    *output = input[0];
+  }
+}
 
-  // 3. 分配device端内存
-  float* d_input, *d_output;
-  cudaMalloc((void **)&d_input, elemNums * sizeof(float));
-  cudaMalloc((void **)&d_output, sizeof(float));
+#define BLOCK_DIM 256
 
-  // 4. 拷贝host端的input到device端
-  cudaMemcpy(d_input, h_input, elemNums * sizeof(float), cudaMemcpyHostToDevice);
+__global__ void SharedMemorySumReducetionKernel(float* input, float* output) {
+  __shared__ float input_s[BLOCK_DIM];
+  unsigned int t = threadIdx.x;
+  input_s[t] = input[t] + input[t + BLOCK_DIM];
+  for (unsigned int stride = blockDim.x / 2; stride >= 1; stride /= 2) {
+    __syncthreads();
+    if (threadIdx.x < stride) {
+      input_s[t] += input_s[t + stride];
+    }
+    if (threadIdx.x == 0) {
+      *output = input_s[0];
+    }
+  }
+}
 
-  // 5. 启动内核
-  SimpleSumReductionKernel<<<gridSize, blockSize>>>(d_input, d_output);
+__global__ void SegmentedSumReductionKernel(float* input, float* output) {
+  __shared__ float input_s[BLOCK_DIM];
+  unsigned int segment = 2 * blockDim.x * blockIdx.x;
+  unsigned int i = segment + threadIdx.x;
+  unsigned int t = threadIdx.x;
+  input_s[t] = input[i] + input[i + BLOCK_DIM];
+  for(unsigned int stride = blockDim.x / 2; stride >= 1; stride /= 2) {
+    __syncthreads();
+    if (t < stride) {
+      input_s[t] += input_s[t + stride];
+    }
+  }
+  if (t == 0) {
+    atomicAdd(output, input_s[0]);
+  }
+}
 
-  // 6. 拷贝device端output到host端
-  cudaMemcpy(h_output, d_output, sizeof(float), cudaMemcpyDeviceToHost);
+#define COARSE_FACTOR 2
 
-  std::cout << *h_output << std::endl;
-
-  // 7. 释放内存
-  free(h_input);
-  free(h_output);
-  cudaFree(d_input);
-  cudaFree(d_output);
-
-  return 0;
+__global__ void CoarsenedSumReductionKernel(float* input, float* output) {
+  __shared__ float input_s[BLOCK_DIM];
+  unsigned int segment = COARSE_FACTOR * 2 * blockDim.x * blockIdx.x;
+  unsigned int i = segment + threadIdx.x;
+  unsigned int t = threadIdx.x;
+  float sum = input[i];
+  for (unsigned int tile = 1; tile < COARSE_FACTOR * 2; ++tile) {
+    sum += input[i + tile * BLOCK_DIM];
+  }
+  input_s[t] = sum;
+  for (unsigned int stride = blockDim.x / 2; stride >= 1; stride /= 2) {
+    __syncthreads();
+    if (t < stride) {
+      input_s[t] += input_s[t + stride];
+    }
+  }
+  if (t == 0) {
+    atomicAdd(output, input_s[0]);
+  }
 }
